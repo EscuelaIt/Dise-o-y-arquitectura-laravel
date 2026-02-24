25662909b3e002e50af8185b2dab3a55eb7f48cc
midesweb <malvarez@desarrolloweb.com>
2026-02-24 09:27:01 +0100

    binding con atributo

--- app/Contracts/CertificateCreator.php
+++ app/Contracts/CertificateCreator.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Contracts;
 
+use App\Services\PDFCertificateCreator;
+use Illuminate\Container\Attributes\Bind;
+
+#[Bind(PDFCertificateCreator::class)]
 interface CertificateCreator
 {
     public function generate($user, $course): string;
